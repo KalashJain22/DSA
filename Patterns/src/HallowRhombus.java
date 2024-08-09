5eea2c9a1108c4803ab64395011ac98e00e4a0cc
Kalash Jain <kalashjain2201@gmail.com>
2024-08-09 14:36:38 +0530

    Create HallowRhombus.java

---
+++ Patterns/src/HallowRhombus.java
@@ -0,0 +1,17 @@
+public class HollowRhombus {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < (4-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j = 0; j < 4; j++) {
+                if(i == 0 || i == 4 || j == 0 || j == 3) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
