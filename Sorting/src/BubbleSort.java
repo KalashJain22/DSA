bddd479b7b2d64184ff61b07a3e7be62e1e69540
Kalash Jain <kalashjain2201@gmail.com>
2024-08-16 21:54:50 +0530

    Create BubbleSort.java

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 6, 3, 5, 4};
+
+        System.out.println("After sorting in increasing order: " + bubble(arr));
+    }
+
+    public static String bubble(int[] arr) {
+        for(int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return Arrays.toString(arr);
+    }
+}
